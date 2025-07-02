f925b205a1812aed68e210fd2b6e638afb905969
Julian Mounce <78062840+Jmounce4@users.noreply.github.com>
2025-07-02 16:16:38 -0400

    Update 64_MinimumPathSum.py

--- 64_MinimumPathSum.py
+++ 64_MinimumPathSum.py
@@ -11,22 +11,27 @@ class Solution(object):
 
         def helper(v, h):
 
+
+            #This path cost is already calculated
             if (v, h) in memoryDict:
                 return memoryDict[(v, h)]
-            #Reaches end, return current sum
+            #Reaches end, return final space cost
             if v == m-1 and h == n-1:
                 return grid[v][h]
             
             minimum = 100000
 
+            #If can go this direction, find path with minimum cost
             if v < m-1:
                 minimum = min(minimum, helper(v+1, h))
 
             if h < n-1:
                 minimum = min(minimum, helper(v, h+1))
 
+            #Stores Coordinate with minimum path
             memoryDict[(v,h)] = grid[v][h] + minimum
 
             return memoryDict[(v,h)]
 
         return helper(0,0)
+            
