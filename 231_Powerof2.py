390f190448fd5a16206f9e2f7e311f854cebc8d2
Julian Mounce <78062840+Jmounce4@users.noreply.github.com>
2025-08-10 12:45:17 -0400

    Create 231_Powerof2.py

---
+++ 231_Powerof2.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        
+        if n < 1:
+            return False
+        
+        while n % 2 == 0:
+            n //= 2
+
+        if n == 1:
+            return True
+        else:
+            return False
