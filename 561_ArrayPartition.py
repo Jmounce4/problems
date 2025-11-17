b7d3fddadfff481dcbca4b6cb47491f51e688d4a
Julian Mounce <78062840+Jmounce4@users.noreply.github.com>
2025-11-16 22:55:05 -0500

    Create 561_ArrayPartition.py

---
+++ 561_ArrayPartition.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        total = 0
+
+        for i in range(len(nums)):
+            if (i%2 == 0) or i == 0:
+                total+=nums[i]
+        return total
+
