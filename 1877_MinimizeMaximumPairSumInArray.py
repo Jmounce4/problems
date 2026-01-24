014035e0e32ad2c3b4d157a4b71088cb7ea4d42f
Julian Mounce <78062840+Jmounce4@users.noreply.github.com>
2026-01-24 13:24:09 -0500

    Create 1877_MinimizeMaximumPairSumInArray.py

---
+++ 1877_MinimizeMaximumPairSumInArray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        sums = []
+
+        for i in range(len(nums)/2):
+            sums.append(nums[i]+nums[len(nums)-i-1])
+
+        return max(sums)
